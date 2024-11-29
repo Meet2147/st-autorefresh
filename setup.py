d412d1af1a2ec9f9448e650448a557a0d5375a64
Meet Jethwa <meetjethwa3@gmail.com>
2024-11-29 23:20:24 +0530

    added changelog

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
-        "Framework :: Streamlit",
+        
     ],
     python_requires=">=3.6",
     keywords="streamlit, autorefresh, real-time, component",
